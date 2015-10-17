4cb2b647a265f3f4598300d640f3e8a381a6953d
hoover456 <hoover852@yahoo.com>
2015-10-17 07:24:20 -0400

    May work with heroku now

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, render_template
-from flask.ext.heroku import Heroku
+## from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-heroku = Heroku(app)
+## heroku = Heroku(app)
 
 app.config["DEBUG"] = True  # Only include this while you are testing your app
 
@@ -18,5 +18,6 @@ def results():
 def page_not_found(error):
     return "Sorry, this page was not found.", 404
 
-##if __name__ == "__main__":
-    ##app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
