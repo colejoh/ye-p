a6ce5bf411d121a55eb7c97e661a535bcbe174f6
Riley Barnett <barnetr@purdue.edu>
2015-10-17 07:59:28 -0400

    Fixed IMG assets for Flask

--- sample.py
+++ sample.py
@@ -1,15 +1,15 @@
-"""
-Yelp API v2.0 code sample.
-This program demonstrates the capability of the Yelp API version 2.0
-by using the Search API to query for businesses by a search term and location,
-and the Business API to query additional information about the top result
-from the search query.
-Please refer to http://www.yelp.com/developers/documentation for the API documentation.
-This program requires the Python oauth2 library, which you can install via:
-`pip install -r requirements.txt`.
-Sample usage of the program:
-`python sample.py --term="bars" --location="San Francisco, CA"`
-"""
+##"""
+##Yelp API v2.0 code sample.
+##This program demonstrates the capability of the Yelp API version 2.0
+##by using the Search API to query for businesses by a search term and location,
+##and the Business API to query additional information about the top result
+##from the search query.
+##Please refer to http://www.yelp.com/developers/documentation for the API documentation.
+##This program requires the Python oauth2 library, which you can install via:
+##`pip install -r requirements.txt`.
+##Sample usage of the program:
+##`python sample.py --term="bars" --location="San Francisco, CA"`
+##"""
 import argparse
 import json
 import pprint
