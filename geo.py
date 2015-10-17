2e6b0b350a5135ef1ec2ac715626b9d540d29ce8
Riley Barnett <barnetr@purdue.edu>
2015-10-17 10:00:16 -0400

    Deleted all the things

--- geo.py
+++
@@ -1,7 +0,0 @@
-import urllib
-from urllib2 import urlopen
-
-ip = urlopen('http://ip.42.pl/raw').read()
-response = urllib.urlopen('http://api.hostip.info/get_html.php?ip=' + ip + '&position=true').read()
-
-print(response)
